3e5c8fe7329c5a859b8e2d50ed4b4513b91a607e
ARNAB HALDER <harnab308@gmail.com>
2024-09-29 11:46:05 +0530

    Updated Binary search, fixed bug

--- binarySearch.py
+++ binarySearch.py
@@ -5,16 +5,20 @@ beg = 0
 end = len(arr) - 1
 found = False
 while not found:
-    index = (beg + end) // 2
-    if arr[index] == target:
-        found = True
-        print("Array: ", arr)
-        print(f'Target {target} element found at {index + 1} position')
+    if target in arr:
+        index = (beg + end) // 2
+        if arr[index] == target:
+            found = True
+            print("Array: ", arr)
+            print(f'Target {target} element found at {index + 1} position')
+            break
+        elif target > arr[index]:
+            beg = index + 1
+        elif target < arr[index]:
+            end = index - 1
+    else:
+        print("Not Present")
+        found = False
         break
-    elif target > arr[index]:
-        beg = index + 1
-    elif target < arr[index]:
-        end = index - 1
-
 
     print(beg, end)
