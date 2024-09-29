b9feae8b4ed78f21e51fd397f9bd5997d0c5cfb8
ARNAB HALDER <harnab308@gmail.com>
2024-09-29 12:24:07 +0530

    fixed issues

--- eq_1_prac.py
+++ eq_1_prac.py
@@ -1,14 +1,14 @@
 x = int(input())
 n = int(input())
-t = -x
+t = x
 s = x
 
 N = 3
 while n > 0:
-    t = t * (x * x) / (N * (N - 1))
+    t = (-t) * (x * x) / (N * (N - 1))
     s += t
 
     N += 2
-    n -=1
+    n -= 1
 
 print(s)
