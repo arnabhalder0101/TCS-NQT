0d8b3f13a531cea26a1fc251df5c9218284b373f
ARNAB HALDER <harnab308@gmail.com>
2024-09-29 12:22:00 +0530

    fixed and corrected

--- eq1.py
+++ eq1.py
@@ -4,7 +4,8 @@ n = int(input("enter range: "))
 x = int(input("Enter X: "))
 sum = x
 t = x
-for i in range(1, n):
+for i in range(1, n+1):
+    print(i)
     t = (-t)*(x*x)/((2*i)*(2*i+1))
     sum += t
 
---
+++ eq_1_prac.py
@@ -0,0 +1,14 @@
+x = int(input())
+n = int(input())
+t = -x
+s = x
+
+N = 3
+while n > 0:
+    t = t * (x * x) / (N * (N - 1))
+    s += t
+
+    N += 2
+    n -=1
+
+print(s)
